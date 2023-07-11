605d3ebd44138971d2810caadb907cf21506d690
ivan-fan-dk <109140290+ivan-fan-dk@users.noreply.github.com>
2023-07-11 21:15:33 +0200

    Add files via upload

---
+++ Arithmetic_Problems/login.js
@@ -0,0 +1,18 @@
+const login = document.getElementById("login")
+login.addEventListener("click", function(){
+    const sqlite3 = require('sqlite3').verbose();
+
+    let db = new sqlite3.Database(':memory:', (err) => {
+        if (err) {
+        return console.error(err.message);
+        }
+        console.log('Connected to the in-memory SQlite database.');
+    });
+
+    db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+    });
+})
